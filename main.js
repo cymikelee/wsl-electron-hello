f0c1b244265d0904817a4815d7a14ae4374eb356
Mike Lee <cymikelee@hotmail.com>
2022-02-06 23:15:35 -0600

    Add OS window lifecycle mgmt + preload version #s

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createWindow () {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('index.html')
-  }
-
-  app.whenReady().then(() => {
-    createWindow()
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
-  
+    
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  // Open a window if none are open (macOS)
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit the app when all windows are closed (Windows & Linux)
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
